cdea9131a0dd6c5fa35af3ce80863b47e18055dc
Somtochukwu Ernest <52139555+Somto811@users.noreply.github.com>
2019-09-08 10:07:33 +0100

    Update lydia_ai_chat_bot.py

--- dbplugins/lydia_ai_chat_bot.py
+++ dbplugins/lydia_ai_chat_bot.py
@@ -47,4 +47,65 @@ async def lydia_disable_enable(event):
             logger.info(session)
             logger.info(add_s(user_id, chat_id, session.id, session.expires))
             await event.edit(f"Lydia AI turned on for [user](tg://user?id={user_id}) in chat: `{chat_id}`")
-        elif input
+        elif input_str == "del":
+            logger.info(remove_s(user_id, chat_id))
+            await event.edit(f"Lydia AI turned off for [user](tg://user?id={user_id}) in chat: `{chat_id}`")
+        elif input_str == "lst":
+            lsts = get_all_s()
+            if len(lsts) > 0:
+                output_str = "Lydia AI enabled users:\n\n"
+                for lydia_ai in lsts:
+                    output_str += f"[user](tg://user?id={lydia_ai.user_id}) in chat `{lydia_ai.chat_id}`\n"
+            else:
+                output_str = "no Lydia AI enabled users / chats. Start by replying `.enacf` to any user in any chat!"
+            if len(output_str) > Config.MAX_MESSAGE_SIZE_LIMIT:
+                with io.BytesIO(str.encode(output_str)) as out_file:
+                    out_file.name = "lydia_ai.text"
+                    await borg.send_file(
+                        event.chat_id,
+                        out_file,
+                        force_document=True,
+                        allow_cache=False,
+                        caption="Lydia AI enabled users",
+                        reply_to=event
+                    )
+            else:
+                await event.edit(output_str)
+        else:
+            await event.edit("Reply To User Message to Add / Delete them from Lydia Auto-Chat.")
+    else:
+        await event.edit("Reply To A User's Message to Add / Delete them from Lydia Auto-Chat.")
+
+
+@borg.on(admin_cmd(incoming=True))
+async def on_new_message(event):
+    if event.chat_id in Config.UB_BLACK_LIST_CHAT:
+        return
+    if Config.LYDIA_API is None:
+        return
+    if not event.media:
+        user_id = event.from_id
+        chat_id = event.chat_id
+        s = get_s(user_id, chat_id)
+        if s is not None:
+            session_id = s.session_id
+            session_expires = s.session_expires
+            query = event.text
+            # Check if the session is expired
+            # If this method throws an exception at this point,
+            # then there's an issue with the API, Auth or Server.
+            if session_expires < time():
+                # re-generate session
+                session = api_client.create_session()
+                logger.info(session)
+                session_id = session.id
+                session_expires = session.expires
+                logger.info(add_s(user_id, chat_id, session_id, session_expires))
+            # Try to think a thought.
+            try:
+                async with event.client.action(event.chat_id, "typing"):
+                    await asyncio.sleep(1)
+                    output = api_client.think_thought(session_id, query)
+                    await event.reply(output)
+            except cf.exception.CoffeeHouseError as e:
+                logger.info(str(e))
