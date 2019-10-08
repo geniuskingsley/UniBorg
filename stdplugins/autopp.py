85f45f4b31d326773a9f5fd698c12856a710072e
Cee Jay <52139555+Somto811@users.noreply.github.com>
2019-10-08 16:52:01 +0100

    Update autopp.py

--- stdplugins/autopp.py
+++ stdplugins/autopp.py
@@ -1,51 +1,84 @@
-"""
-Time In Profile Pic.....
-Command: `.autopp`
-:::::Credit Time::::::
-1) Coded By: @s_n_a_p_s
-2) Ported By: @loxxi
-3) End Game Help By: @spechide
-#curse: who ever edits this credit section will goto hell
-⚠️DISCLAIMER⚠️
-USING THIS PLUGIN CAN RESULT IN ACCOUNT BAN. WE DONT CARE ABOUT BAN, SO WE ARR USING THIS.
-"""
 import os
+
 from datetime import datetime
+
+from pytz import timezone
+
 from PIL import Image, ImageDraw, ImageFont
+
 from pySmartDL import SmartDL
+
 from telethon.tl import functions
+
 from uniborg.util import admin_cmd
+
 import asyncio
+
 import shutil
 
-FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
+VERY_PIC = "https://pes2020.sirv.com/mn.jpg" 
 
-@borg.on(admin_cmd(pattern="autopp ?(.*)"))
+@borg.on(admin_cmd(pattern="autopp"))
+
 async def autopic(event):
-    downloaded_file_name = "./downloads/photo_2019-10-08_14-13-03.jpg"
-    downloader = SmartDL(Config.loxxi_crew, downloaded_file_name, progress_bar=True)
+
+    downloaded_file_name = "./DOWNLOADS/original_pic.png"
+
+    downloader = SmartDL(VERY_PIC, downloaded_file_name, progress_bar=False)
+
     downloader.start(blocking=False)
+
     photo = "photo_pfp.png"
+
     while not downloader.isFinished():
+
         place_holder = None
-    counter = -30
+
+    counter = -5
+
     while True:
+
         shutil.copy(downloaded_file_name, photo)
+
         im = Image.open(photo)
-        file_test = im.rotate(counter, expand=False).save(photo, "PNG")
-        current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \n  Time: %H:%M:%S \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
+
+        file_test = im.save(photo, "PNG")
+
+        now_utc = datetime.now(timezone('UTC'))
+
+        now_asia = now_utc.astimezone(timezone('Asia/Kolkata'))
+     
+
+        current_time = now_asia.strftime("°°°°°°°°°°°°°°°\n Time: %I:%M:%p\n Date: %d-%m-%y\n°°°°°°°°°°°°°°°")
+
+
         img = Image.open(photo)
+
         drawn_text = ImageDraw.Draw(img)
-        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 35)
-        drawn_text.text((200, 250), current_time, font=fnt, fill=(255, 255, 0))
+
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
+
+        drawn_text.text((200,350), current_time, font=fnt, fill=(19, 157, 232))
+
         img.save(photo)
+
         file = await event.client.upload_file(photo)  # pylint:disable=E0602
+
         try:
+
             await event.client(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
+
                 file
+
             ))
+
             os.remove(photo)
-            counter -= 30
-            await asyncio.sleep(60)
+
+            counter -= 5
+
+            await asyncio.sleep(70)
+
         except:
+
             return
