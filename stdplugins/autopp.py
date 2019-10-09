14b963d4570fd82635e0eb35fc9b81f617b471b4
Cee Jay <52139555+Somto811@users.noreply.github.com>
2019-10-09 09:09:36 +0100

    Update autopp.py

--- stdplugins/autopp.py
+++ stdplugins/autopp.py
@@ -1,84 +1,40 @@
 import os
-
 from datetime import datetime
-
-from pytz import timezone
-
 from PIL import Image, ImageDraw, ImageFont
-
 from pySmartDL import SmartDL
-
 from telethon.tl import functions
-
-from uniborg.util import admin_cmd
-
 import asyncio
-
 import shutil
 
 FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
-VERY_PIC = "https://telegra.ph/file/aec1ac69db136b4edf814.jpg" 
 
-@borg.on(admin_cmd(pattern="autopp"))
 
+@borg.on(admin_cmd(pattern="autopic")), outgoing=True)) 
 async def autopic(event):
-
-    downloaded_file_name = "./DOWNLOADS/original_pic.png"
-
-    downloader = SmartDL(VERY_PIC, downloaded_file_name, progress_bar=False)
-
+    downloaded_file_name = "userbot/original_pic.png"
+    downloader = SmartDL(Var.DOWNLOAD_PFP_URL_CLOCK, downloaded_file_name, progress_bar=False)
     downloader.start(blocking=False)
-
-    photo = "photo_pfp.png"
-
+    photo = "userbot/photo_pfp.png"
     while not downloader.isFinished():
-
         place_holder = None
-
     counter = -5
-
     while True:
-
         shutil.copy(downloaded_file_name, photo)
-
         im = Image.open(photo)
-
-        file_test = im.save(photo, "PNG")
-
-        now_wat = datetime.now(timezone('WAT'))
-
-        now_africa = now_wat.astimezone(timezone('Etc/GMT-1'))
-     
-
-        current_time = now_asia.strftime("°°°°°°°°°°°°°°°\n Time: %I:%M:%p\n Date: %d-%m-%y\n°°°°°°°°°°°°°°°")
-
-
+        file_test = im.rotate(counter, expand=False).save(photo, "PNG")
+        current_time = datetime.now().strftime("°°°°°°°°°°°°°°°°° \n  Time: %H:%M:%S \n  Date: %d.%m.%y \n°°°°°°°°°°°°°°°°°")
         img = Image.open(photo)
-
         drawn_text = ImageDraw.Draw(img)
-
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
-
-        drawn_text.text((200,350), current_time, font=fnt, fill=(19, 157, 232))
-
+        drawn_text.text((50, 250), current_time, font=fnt, fill=(255, 255, 255))
         img.save(photo)
-
-        file = await event.client.upload_file(photo)  # pylint:disable=E0602
-
+        file = await bot.upload_file(photo)  # pylint:disable=E0602
         try:
-
-            await event.client(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
-
+            await bot(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
                 file
-
             ))
-
             os.remove(photo)
-
             counter -= 5
-
-            await asyncio.sleep(70)
-
+            await asyncio.sleep(65)
         except:
-
             return
