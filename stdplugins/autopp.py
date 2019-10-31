5ac564d3c95ac1698e7d440e7730cd9ef3917b62
Cee Jay <52139555+Somto811@users.noreply.github.com>
2019-10-31 23:46:01 +0100

    Update autopp.py

--- stdplugins/autopp.py
+++ stdplugins/autopp.py
@@ -4,39 +4,37 @@ from PIL import Image, ImageDraw, ImageFont
 from pySmartDL import SmartDL
 from telethon.tl import functions
 from uniborg.util import admin_cmd
-
 import asyncio
 import shutil
 
-FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
-
+FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
 
 @borg.on(admin_cmd(pattern="autopic ?(.*)"))
 async def autopic(event):
     downloaded_file_name = "./DOWNLOADS/original_pic.png"
-    downloader = SmartDL(Config.RAVANA_LEELA, downloaded_file_name, progress_bar=True)
+    downloader = SmartDL(Config.LOXXI_CREW, downloaded_file_name, progress_bar=True)
     downloader.start(blocking=False)
-    photo = "DOWNLOADS/photo_pfp.png"
+    photo = "photo_pfp.png"
     while not downloader.isFinished():
         place_holder = None
-    counter = -5
+    counter = -30
     while True:
         shutil.copy(downloaded_file_name, photo)
         im = Image.open(photo)
         file_test = im.rotate(counter, expand=False).save(photo, "PNG")
-        current_time = datetime.now().strftime("°°°°°°°°°°°°°°°°° \n  Time: %H:%M:%S \n  Date: %d.%m.%y \n°°°°°°°°°°°°°°°°°")
+        current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \n  Time: %H:%M:%S \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
-        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
-        drawn_text.text((50, 250), current_time, font=fnt, fill=(255, 255, 255))
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 35)
+        drawn_text.text((200, 250), current_time, font=fnt, fill=(255, 255, 0))
         img.save(photo)
-        file = await bot.upload_file(photo)  # pylint:disable=E0602
+        file = await event.client.upload_file(photo)  # pylint:disable=E0602
         try:
-            await bot(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
+            await event.client(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
                 file
             ))
             os.remove(photo)
-            counter -= 5
-            await asyncio.sleep(65)
+            counter -= 30
+            await asyncio.sleep(60)
         except:
             return
